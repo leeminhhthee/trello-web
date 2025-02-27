e8a327eb0cebe187ebf44c2f9c0e4cddf37d8f0e
leeminhhthee <minhthe22122003@gmail.com>
2025-02-28 00:25:40 +0700

    Refactor Project Structure

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,37 +1,15 @@
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
-import ModeSelect from '../../components/ModeSelect'
+import AppBar from '../../components/AppBar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
 
 function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-      <Box sx={{
-        backgroundColor: 'primary.light',
-        width: '100%',
-        height: (theme) => theme.trello.appBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        <ModeSelect />
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.dark',
-        width: '100%',
-        height: (theme) => theme.trello.boardBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board bar
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.main',
-        width: '100%',
-        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board Content
-      </Box>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </Container>
   )
 }
