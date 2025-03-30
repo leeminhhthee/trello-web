678c0faa05897e1617a52de53d05a3f7ff70a5eb
leeminhhthee <minhthe22122003@gmail.com>
2025-03-31 00:11:57 +0700

    update_UXUI

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -37,6 +37,8 @@ function Card({ card }) {
         boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
         overflow: 'unset',
         display: card?.FE_PlaceholderCard ? 'none' : 'block',
+        border: '1px solid transparent',
+        '&:hover': { borderColor: (theme) => theme.palette.primary.main }
         // overflow: card?.FE_Placeholder ? 'hidden' : 'unset',
         // height: card?.FE_Placeholder ? '0px' : 'unset'
       }}>
